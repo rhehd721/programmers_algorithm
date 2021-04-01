d4aa982380a80162ca4243126a9ed2cb84c61030
알파카 <rhehd721@naver.com>
2021-04-01 17:03:40 +0900

    프로그래머스 알고리즘

--- Sort_LargestNum/test.py
+++ Sort_LargestNum/test.py
@@ -1,43 +1,60 @@
+# 아니 이것도 효율성이 아나오는건 너무한거 아니야고....
+
+
 def solution(people, limit):
     answer = 0
+    PointStrat = 0
+    PointEnd = len(people)
 
-    people.sort(reverse=1)
+    ExitNum = 0
 
-    while(1):
-        for i in (0, len(people) -1):
-            check = 0
-            tttt = 0
-            if(people[i] != -1):
-                tttt = 0
-                weight = people[i]
-                num = i
-                for j in (i, len(people) -1):
-                    if (people[j] == -1):
-                        continue
-                    else:
-                        if (weight + people[j] > limit):
-                            continue
-                        else:
-                            people[i] = -1
-                            people[j] = -1
-                            check = 1
-                            break
-            else:
-                tttt = 1
-        if(check):
-            print("two exit")
-        else:
-            people[num] = -1
-            print("one exit")
-        if(tttt):
-            print("answer : ", answer)
-            return answer
+
+    people.sort(reverse=1)  
+
+    while(PointStrat <= PointEnd):
         answer += 1
-            
-
+        weight = people[PointStrat]
+        # if(weight == 0):
+        #     print("answer : ", answer -1 )
+        #     return answer
+        ExitNum = 1
+        for i in range(PointStrat + 1, PointEnd):
+            # print(i)
+            # print(weight)
+            weight2 = weight + people[i]
+            if (weight2 > limit ):
+                continue
+            else:
+                if(people[i] == 0):
+                    continue
+                else:
+                    people[i] = 0
+                    people[PointStrat] = 0
+                    ExitNum = 2
+                    break
+        if(ExitNum == 2):
+            ExitNum = 0
+        else:
+            people[PointStrat] = 0
+        # for i in people:
+        #     print(i)
+        while(1):
+            if(people[PointStrat] == 0):
+                PointStrat += 1
+                if(PointEnd -1 == PointStrat):
+                    if(people[PointStrat] == 0):
+                        print("answer : ", answer)
+                        return answer
+                    else:
+                        print("answer : ", answer + 1)
+                        return answer + 1
+            else:
+                break
+    
     print("answer : ", answer)
     return answer
 
+# main
 people = [70, 50, 80, 50]
 limit = 100
 solution(people, limit) # 3
