13fb3f5e67362ab475b04dd5e5e09fe5d74112f2
알파카 <rhehd721@naver.com>
2021-04-04 22:39:01 +0900

    프로그래머스 알고리즘

--- Heap_scoville/mainCopy.py
+++ Heap_scoville/mainCopy.py
@@ -1,45 +1,24 @@
+# -*- coding: utf-8 -*- 
+# 제한 사항
+# scoville의 길이는 2 이상 1,000,000 이하입니다.
+# K는 0 이상 1,000,000,000 이하입니다.
+# scoville의 원소는 각각 0 이상 1,000,000 이하입니다.
+# 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
+
+# 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+
+
 def solution(scoville, K):
-    Check = 0
-    # 처음에 정렬되어있지 않을 수 있다
-    scoville.sort()
+    answer = 0
     
-    # 효율을 위해 0값들을 지워주자
-    if (scoville[0] == 0):
-        scoville.remove(0)
-        # 제일 작은수가 이미 K를 넘긴경우 종료하여주자(0이 존재했었으니 적어도 1 이상이다) ################ 망함(같은숫자들은 하나만 남기고 지워야한다)
-        if (scoville[0] >= K):
-            return 1
-    # 제일 작은수가 이미 K를 넘긴경우 종료하여주자
-    elif (scoville[0] >= K):
-        return 0
+    scoville.sort()
 
-    # list가 2개 이상이면 반복문을 실행해라
-    while (len(scoville) >= 2):
-        
-        # 제일작은 두개로 작업해라
-        scoville[1] = scoville[0] + (scoville[1] * 2)
-        # 더해준 숫자는 없애준다
-        del scoville[0]
-        # 섞은회수 증가!
-        Check += 1
+    while(1):
+        new = scoville[0] + ( scoville[1] * 2 )
 
-        # 정렬해라
-        scoville.sort()
+    
 
-        # 섞은다음부터 커지게되면 종료해라
-        if (scoville[0] >= K):
-            break
-        # 더이상 더할 두개가 없어졌다면
-        elif (len(scoville) == 1):
-            Check = 0
-            break
-
-    # K를 초과하지 못할경우 -1
-    if(Check == 0):
-        answer = -1
-    # K를 초과할경우 섞은 횟수
-    else:
-        answer = Check
+    print( 'answer : ' , answer)
     return answer
 
 print(solution([1, 2, 3, 9, 10, 12], 7))
@@ -50,5 +29,3 @@ print(solution([0, 1000000000], 1000000000))
 print(solution([0, 0,0,0], 1000000000))
 print(solution([100000000,100000000,100000000], 10000))
 print(solution([8, 2, 3, 9, 10, 12], 7))
-print(solution([0, 0,0,0, 1000], 100))
-
